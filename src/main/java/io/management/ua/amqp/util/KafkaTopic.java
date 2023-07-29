77f1a09f5a40c7904e47332494439ee63547947e
Nicknameee <igrunka1991@gmail.com>
2023-07-29 13:59:04 +0300

    KafkaTopics

--- src/main/java/io/management/ua/amqp/util/KafkaTopic.java
+++ src/main/java/io/management/ua/amqp/util/KafkaTopic.java
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class KafkaTopic {
-    public static String AUTHENTICATION = "authentication";
+    public static final String AUTHENTICATION = "authentication";
 }
