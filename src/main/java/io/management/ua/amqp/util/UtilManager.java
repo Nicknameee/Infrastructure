3df57fe72432822a4cc59d4567ab99a4a0b670ac
Nicknameee <igrunka1991@gmail.com>
2023-07-29 14:12:02 +0300

    Jackson

--- src/main/java/io/management/ua/amqp/util/UtilManager.java
+++ src/main/java/io/management/ua/amqp/util/UtilManager.java
@@ -2,12 +2,14 @@ package io.management.ua.amqp.util;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class UtilManager {
     public static ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true);
+        objectMapper.registerModule(new JavaTimeModule());
 
         return objectMapper;
     }
