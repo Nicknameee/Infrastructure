4ff7d1466b79459288e492e613fa4081a1e2022b
Nicknameee <igrunka1991@gmail.com>
2023-08-06 23:35:20 +0300

    Utility

--- src/main/java/io/management/ua/amqp/models/KafkaTopic.java
+++ src/main/java/io/management/ua/amqp/models/KafkaTopic.java
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class KafkaTopic {
-    public static final String USER_APPROVAL_TOPIC = "USER_APPROVAL_TOPIC";
+    public static final String CERTIFICATION_TOPIC = "certification";
 }
