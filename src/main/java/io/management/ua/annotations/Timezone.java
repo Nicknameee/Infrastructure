8564a6f4b1f8c87052aca1e2ed92322188e3a61f
Nicknameee <igrunka1991@gmail.com>
2023-08-08 22:24:02 +0300

    Utility

--- src/main/java/io/management/ua/annotations/Timezone.java
+++ src/main/java/io/management/ua/annotations/Timezone.java
@@ -2,6 +2,7 @@ package io.management.ua.annotations;
 
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -13,4 +14,5 @@ import java.lang.annotation.Target;
 public @interface Timezone  {
     String message() default "Timezone is invalid";
     Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default { };
 }
