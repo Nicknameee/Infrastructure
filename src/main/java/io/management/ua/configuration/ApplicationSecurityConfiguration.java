7aa25cae7fc5a3f2b8413f6dd13551b73fd5ef33
Nicknameee <igrunka1991@gmail.com>
2023-08-08 22:18:23 +0300

    Utility

--- src/main/java/io/management/ua/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/io/management/ua/configuration/ApplicationSecurityConfiguration.java
@@ -54,7 +54,7 @@ public class ApplicationSecurityConfiguration {
         http
                 .authorizeRequests()
                 .antMatchers("/stomp/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/login", "/logout", "/util/**", "**/allow/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/login", "/logout", "/sign/up", "/utility/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
