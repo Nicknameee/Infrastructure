380624a9f92b1e3bdd959e2c1bc1dca9689f7c8a
Nicknameee <igrunka1991@gmail.com>
2023-08-08 23:57:29 +0300

    Build

---
+++ src/main/java/io/management/ua/amqp/KafkaTemplateTool.java
@@ -0,0 +1,16 @@
+package io.management.ua.amqp;
+
+import io.management.ua.amqp.models.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaTemplateTool<T, Y> {
+    private final KafkaTemplate<T, Message<Y>> kafkaTemplate;
+
+    public void produce(String topic, Y message) {
+        kafkaTemplate.send(topic, new Message<>(message));
+    }
+}
