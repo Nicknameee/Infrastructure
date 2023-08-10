ff86362d81e5856f242e054fcdbc1253d786955a
Nicknameee <igrunka1991@gmail.com>
2023-08-10 19:55:02 +0300

    Handler of exceptions refactoring

--- src/main/java/io/management/ua/amqp/models/KafkaTopic.java
+++ src/main/java/io/management/ua/amqp/models/KafkaTopic.java
@@ -6,5 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class KafkaTopic {
-    public static final String CERTIFICATION_TOPIC = "certification";
+    public static final String CERTIFICATION_REQUEST_TOPIC = "certification_request";
+    public static final String CERTIFICATION_RESPONSE_TOPIC = "certification_response";
+
 }
