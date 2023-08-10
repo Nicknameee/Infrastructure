c13f883e21e94daf2eec00fcec6f635c07333803
Nicknameee <igrunka1991@gmail.com>
2023-08-10 18:19:54 +0300

    Message structure fully refactored

--- src/main/java/io/management/ua/amqp/models/Message.java
+++ src/main/java/io/management/ua/amqp/models/Message.java
@@ -15,8 +15,12 @@ public class Message implements Serializable {
     private String json;
     private String implementation;
 
-    public Message(Object object) throws JsonProcessingException {
-        this.json = UtilManager.objectMapper().writeValueAsString(object);
+    public Message(Object object) {
+        try {
+            this.json = UtilManager.objectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         this.implementation = object.getClass().getName();
     }
 }
