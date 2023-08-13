b3e7c086a2ef17995ab232b493bb268edf3a3217
Nicknameee <igrunka1991@gmail.com>
2023-08-14 00:49:03 +0300

    Exceptions

--- src/main/java/io/management/ua/exceptions/AccessDeniedException.java
+++ src/main/java/io/management/ua/exceptions/AccessDeniedException.java
@@ -1,11 +1,10 @@
 package io.management.ua.exceptions;
 
+import io.management.ua.utility.TimeUtil;
 import org.springframework.http.HttpStatus;
 
-import java.time.ZonedDateTime;
-
 public class AccessDeniedException extends GlobalException {
-    public AccessDeniedException(HttpStatus httpStatus, ZonedDateTime exceptionTime, String exception) {
-        super(httpStatus, exceptionTime, exception);
+    public AccessDeniedException(String exception) {
+        super(HttpStatus.CONFLICT, TimeUtil.getCurrentDateTime(), exception);
     }
 }
