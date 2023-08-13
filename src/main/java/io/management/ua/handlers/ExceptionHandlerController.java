87ca93811600d33d6646d57a6f0f634069b2ac99
Nicknameee <igrunka1991@gmail.com>
2023-08-14 01:59:48 +0300

    Exception handler updates

--- src/main/java/io/management/ua/handlers/ExceptionHandlerController.java
+++ src/main/java/io/management/ua/handlers/ExceptionHandlerController.java
@@ -1,13 +1,19 @@
 package io.management.ua.handlers;
 
 import io.management.ua.exceptions.GlobalException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -22,6 +28,13 @@ public class ExceptionHandlerController {
                     .getConstraintViolations()
                     .stream()
                     .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toList()));
+        } else if (e instanceof MethodArgumentNotValidException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception
+                            .getBindingResult()
+                            .getFieldErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .collect(Collectors.toList()));
         }
 
