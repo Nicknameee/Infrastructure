6a810ff33f01443620d8d84da5d668f30d6dc7ce
Nicknameee <igrunka1991@gmail.com>
2023-08-14 02:12:27 +0300

    ExceptionHandlerController

--- src/main/java/io/management/ua/handlers/ExceptionHandlerController.java
+++ src/main/java/io/management/ua/handlers/ExceptionHandlerController.java
@@ -1,19 +1,17 @@
 package io.management.ua.handlers;
 
 import io.management.ua.exceptions.GlobalException;
+import io.management.ua.utility.TimeUtil;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -24,18 +22,29 @@ public class ExceptionHandlerController {
         if (e instanceof GlobalException exception) {
             return ResponseEntity.status(exception.getHttpStatus()).body(exception);
         } else if (e instanceof ConstraintViolationException exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception
-                    .getConstraintViolations()
-                    .stream()
-                    .map(ConstraintViolation::getMessage)
-                    .collect(Collectors.toList()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GlobalException
+                    .builder()
+                    .exceptionTime(TimeUtil.getCurrentDateTime())
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .exception(exception
+                            .getConstraintViolations()
+                            .stream()
+                            .map(ConstraintViolation::getMessage)
+                            .toList()
+                            .toString()));
         } else if (e instanceof MethodArgumentNotValidException exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GlobalException
+                    .builder()
+                    .exceptionTime(TimeUtil.getCurrentDateTime())
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .exception(exception
                             .getBindingResult()
                             .getFieldErrors()
-                    .stream()
-                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                    .collect(Collectors.toList()));
+                            .stream()
+                            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                            .filter(Objects::nonNull)
+                            .toList()
+                            .toString()));
         }
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
