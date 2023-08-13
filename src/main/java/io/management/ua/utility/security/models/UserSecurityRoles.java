c1a7bd644c53d0779fb4e6d071a0eea7a3628380
Nicknameee <igrunka1991@gmail.com>
2023-08-13 23:28:52 +0300

    Roles

--- src/main/java/io/management/ua/utility/security/models/UserSecurityRoles.java
+++ src/main/java/io/management/ua/utility/security/models/UserSecurityRoles.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 @Getter
 public enum UserSecurityRoles {
-    ROLE_USER(UserSecurityPermissions.getUserAuthorities()),
+    ROLE_OPERATOR(UserSecurityPermissions.getUserAuthorities()),
     ROLE_CUSTOMER(UserSecurityPermissions.getCustomerAuthorities()),
     ROLE_MANAGER(UserSecurityPermissions.getManagerAuthorities());
 
