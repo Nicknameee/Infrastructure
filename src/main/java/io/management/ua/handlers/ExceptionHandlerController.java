96a6f83c0ae73600139310a652c5e9dc55aa78c2
Nicknameee <igrunka1991@gmail.com>
2023-08-14 01:23:01 +0300

    Exception handler amending xD

--- src/main/java/io/management/ua/handlers/ExceptionHandlerController.java
+++ src/main/java/io/management/ua/handlers/ExceptionHandlerController.java
@@ -6,6 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
@@ -13,6 +17,12 @@ public class ExceptionHandlerController {
     public ResponseEntity<?> handleApplicationExceptions(Exception e) {
         if (e instanceof GlobalException exception) {
             return ResponseEntity.status(exception.getHttpStatus()).body(exception);
+        } else if (e instanceof ConstraintViolationException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception
+                    .getConstraintViolations()
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toList()));
         }
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
