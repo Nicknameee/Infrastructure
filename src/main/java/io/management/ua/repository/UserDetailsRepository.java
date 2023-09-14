6712afc4cf35cb4515d7784cb0f0bcdd3741f4da
Nicknameee <igrunka1991@gmail.com>
2023-09-15 02:18:54 +0300

    Versioning

--- src/main/java/io/management/ua/repository/UserDetailsRepository.java
+++ src/main/java/io/management/ua/repository/UserDetailsRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface
-UserDetailsRepository extends JpaRepository<UserDetailsModel, Integer> {
+public interface UserDetailsRepository extends JpaRepository<UserDetailsModel, Integer> {
     Optional<UserDetailsModel> findAbstractUserModelByUsername(String username);
 }
