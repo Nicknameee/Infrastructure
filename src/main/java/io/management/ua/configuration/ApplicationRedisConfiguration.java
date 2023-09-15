d426367ecf85a53fbf716a44fff2626f43a0eaa2
Nicknameee <igrunka1991@gmail.com>
2023-09-15 03:19:44 +0300

    Confs

---
+++ src/main/java/io/management/ua/configuration/ApplicationRedisConfiguration.java
@@ -0,0 +1,9 @@
+package io.management.ua.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class ApplicationRedisConfiguration {
+}
