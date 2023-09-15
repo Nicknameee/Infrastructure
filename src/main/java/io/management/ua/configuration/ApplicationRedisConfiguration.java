4cb7f9752e6fc01f037e80eea4cc66b29959d156
Nicknameee <igrunka1991@gmail.com>
2023-09-15 03:10:11 +0300

    Confs

--- src/main/java/io/management/ua/configuration/ApplicationRedisConfiguration.java
+++
@@ -1,9 +0,0 @@
-package io.management.ua.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories
-public class ApplicationRedisConfiguration {
-}
--- src/main/java/io/management/ua/utility/repository/UserDetailsRepository.java
+++ src/main/java/io/management/ua/utility/repository/UserDetailsRepository.java
@@ -2,11 +2,9 @@ package io.management.ua.utility.repository;
 
 import io.management.ua.utility.models.UserDetailsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetailsModel, Integer> {
     Optional<UserDetailsModel> findAbstractUserModelByUsername(String username);
 }
