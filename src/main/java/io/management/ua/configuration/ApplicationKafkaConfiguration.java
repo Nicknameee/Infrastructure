00cf7e8d9589422d59b54d50c6a93a745cf865e2
Nicknameee <igrunka1991@gmail.com>
2023-09-20 15:43:12 +0300

    Allowed packages configuration

--- src/main/java/io/management/ua/configuration/ApplicationKafkaConfiguration.java
+++ src/main/java/io/management/ua/configuration/ApplicationKafkaConfiguration.java
@@ -1,6 +1,7 @@
 package io.management.ua.configuration;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,10 +12,6 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
-
-import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.clients.CommonClientConfigs.RETRIES_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 @Configuration
 public class ApplicationKafkaConfiguration {
@@ -29,21 +26,29 @@ public class ApplicationKafkaConfiguration {
     @Bean
     public ProducerFactory<String, ?> producerFactory() {
         return new DefaultKafkaProducerFactory<>(
-                Map.of(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-                        RETRIES_CONFIG, 0,
-                        BUFFER_MEMORY_CONFIG, 33554432,
-                        KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-                        VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
-                        JsonDeserializer.TRUSTED_PACKAGES, "io.management.ua.amqp.messages.*"
+                Map.of(
+                        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                        ProducerConfig.RETRIES_CONFIG, 0,
+                        ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432,
+                        ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 5000,
+                        ProducerConfig.RECONNECT_BACKOFF_MAX_MS_CONFIG, 3000,
+                        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
+                        JsonDeserializer.TRUSTED_PACKAGES, "io.management.ua.amqp.messages"
                 ));
     }
 
     @Bean
     public ConsumerFactory<String, ?> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+        return new DefaultKafkaConsumerFactory<>(Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, true,
+                ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 1000,
+                ConsumerConfig.RECONNECT_BACKOFF_MAX_MS_CONFIG, 3000,
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
-                JsonDeserializer.TRUSTED_PACKAGES, "io.management.ua.amqp.messages.*"
+                ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 1000,
+                JsonDeserializer.TRUSTED_PACKAGES, "io.management.ua.amqp.messages"
         ));
     }
 }
