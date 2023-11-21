3cf77ca2d29e6d880b006c1831d0a6dbf99557b1
Nicknameee <igrunka1991@gmail.com>
2023-11-21 14:24:59 +0200

    Build dd ddd

---
+++ src/main/java/io/management/ua/utility/models/APIRequestParameterNames.java
@@ -0,0 +1,15 @@
+package io.management.ua.utility.models;
+
+public class APIRequestParameterNames {
+    public static final String weight = "weight";
+    public static final String length = "length";
+    public static final String width = "width";
+    public static final String height = "height";
+    public static final String packing = "packing";
+    public static final String itemsNumber = "itemsNumber";
+    public static final String itemsPackage = "itemsPackage";
+    public static final String citySender = "citySender";
+    public static final String cityReceiver = "cityReceiver";
+    public static final String cost = "cost";
+    public static final String API = "api";
+}
