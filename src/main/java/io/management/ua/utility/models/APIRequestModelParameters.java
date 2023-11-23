55c34d01dc424ed4d423f3dc6cd3b0ace12fa8d1
Nicknameee <igrunka1991@gmail.com>
2023-11-23 19:53:41 +0200

    Build

--- src/main/java/io/management/ua/utility/models/APIRequestModelParameters.java
+++ src/main/java/io/management/ua/utility/models/APIRequestModelParameters.java
@@ -2,11 +2,11 @@ package io.management.ua.utility.models;
 
 import lombok.Data;
 
-import java.util.Map;
+import java.util.HashMap;
 
 @Data
 public class APIRequestModelParameters {
-    private Map<String, Object> parameters;
+    private HashMap<String, Object> parameters;
 
     public Object get(String key) {
         return parameters.get(key);
