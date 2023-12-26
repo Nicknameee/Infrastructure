ee5016078d0ef64122c40e0c48220682e29bb841
Nicknameee <igrunka1991@gmail.com>
2023-12-26 02:27:16 +0200

    Build

--- src/main/java/io/management/ua/utility/models/APIRequestParameterNames.java
+++ src/main/java/io/management/ua/utility/models/APIRequestParameterNames.java
@@ -1,16 +1,17 @@
 package io.management.ua.utility.models;
 
 public class APIRequestParameterNames {
-    public static final String weight = "weight";
-    public static final String length = "length";
-    public static final String width = "width";
-    public static final String height = "height";
-    public static final String packing = "packing";
-    public static final String itemsNumber = "itemsNumber";
-    public static final String itemsPackage = "itemsPackage";
-    public static final String citySender = "citySender";
-    public static final String cityReceiver = "cityRecipient";
-    public static final String cost = "cost";
+    public static final String WEIGHT = "weight";
+    public static final String LENGTH = "length";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String PACKING = "packing";
+    public static final String ITEMS_NUMBER = "itemsNumber";
+    public static final String ITEMS_PACKAGE = "itemsPackage";
+    public static final String CITY_SENDER = "citySender";
+    public static final String CITY_RECEIVER = "cityRecipient";
+    public static final String COST = "cost";
     public static final String API = "api";
-    public static final String departmentType = "departmentType";
+    public static final String CALLED_METHOD = "calledMethod";
+    public static final String DEPARTMENT_TYPE = "departmentType";
 }
