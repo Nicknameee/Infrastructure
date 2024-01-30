074ba74a4626d4fc60ef9176d5ad1ba6a245d19f
Nicknameee <igrunka1991@gmail.com>
2024-01-30 20:26:28 +0200

    Build...

--- src/main/java/io/management/ua/amqp/messages/APIRequestModel.java
+++ src/main/java/io/management/ua/amqp/messages/APIRequestModel.java
@@ -3,9 +3,13 @@ package io.management.ua.amqp.messages;
 import io.management.ua.utility.enums.API;
 import io.management.ua.utility.models.APICallbackModel;
 import io.management.ua.utility.models.APIRequestModelParameters;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class APIRequestModel {
     private API api;
     private APIRequestModelParameters data;
--- src/main/java/io/management/ua/utility/models/HttpServletAddressesModel.java
+++ src/main/java/io/management/ua/utility/models/HttpServletAddressesModel.java
@@ -1,9 +1,13 @@
 package io.management.ua.utility.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class HttpServletAddressesModel {
     private String scheme;
