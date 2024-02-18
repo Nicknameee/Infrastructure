b45bf1d78c7eb2b0a8b2509ff4623c048471c59a
Nicknameee <igrunka1991@gmail.com>
2024-02-18 07:20:13 +0200

    Patterns   $

---
+++ src/main/java/io/management/ua/utility/Pattern.java
@@ -0,0 +1,6 @@
+package io.management.ua.utility;
+
+public class Pattern {
+    public static final String EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String TELEGRAM_USERNAME = "^[a-zA-Z0-9]{5,}$";
+}
