1870fa73b6400379708fa801fc7124d4e16a0920
Nicknameee <igrunka1991@gmail.com>
2024-03-03 15:52:58 +0200

    JdbcTemplate

--- src/main/java/io/management/ua/configuration/ApplicationDatabaseConfiguration.java
+++ src/main/java/io/management/ua/configuration/ApplicationDatabaseConfiguration.java
@@ -10,7 +10,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -74,5 +76,12 @@ public class ApplicationDatabaseConfiguration {
                         databaseFlywayProperties.getUser(),
                         databaseFlywayProperties.getPassword())
                 .load();
+    }
+
+    @Bean
+    @Primary
+    @ConditionalOnBean(name = "databaseDataSource")
+    public JdbcTemplate databaseJdbcTemplate(@Qualifier("databaseDataSource") DataSource databaseDataSource) {
+        return new JdbcTemplate(databaseDataSource);
     }
 }
