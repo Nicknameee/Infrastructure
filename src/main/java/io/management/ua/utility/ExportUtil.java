da570c2e81ee1ec6e47f5613e36349eba59c589a
Nicknameee <igrunka1991@gmail.com>
2024-03-05 13:46:02 +0200

    ExportUtil$$$$$

--- src/main/java/io/management/ua/utility/ExportUtil.java
+++ src/main/java/io/management/ua/utility/ExportUtil.java
@@ -3,7 +3,11 @@ package io.management.ua.utility;
 import io.management.ua.annotations.Export;
 import io.management.ua.exceptions.DefaultException;
 import io.netty.util.internal.StringUtil;
+import org.apache.poi.ss.usermodel.Cell;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class ExportUtil {
@@ -32,5 +36,34 @@ public class ExportUtil {
         }
 
         return filename;
+    }
+
+    @Export
+    public static void setCellValue(Cell cell, Object value) {
+        if (value instanceof String) {
+            cell.setCellValue((String) value);
+        } else if (value instanceof Long) {
+            cell.setCellValue((Long) value);
+        } else if (value instanceof Integer) {
+            cell.setCellValue((Integer) value);
+        } else if (value instanceof BigDecimal) {
+            cell.setCellValue(((BigDecimal) value).doubleValue());
+        } else if (value instanceof Boolean) {
+            cell.setCellValue((Boolean) value);
+        } else if (value instanceof UUID) {
+            cell.setCellValue(value.toString());
+        } else if (value instanceof Map<?, ?> map) {
+            StringBuilder sb = new StringBuilder();
+
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                sb.append(entry.getKey()).append(": ").append(entry.getValue()).append(", ");
+            }
+
+            String mapAsString = sb.substring(0, sb.length() - 2);
+            cell.setCellValue(mapAsString);
+        } else if (value instanceof String[] array) {
+            String arrayAsString = String.join(", ", array);
+            cell.setCellValue(arrayAsString);
+        }
     }
 }
